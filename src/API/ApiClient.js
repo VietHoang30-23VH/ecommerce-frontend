09db2246c1977404fd88b32a0ce3473b96fc35d1
=Xuan Dong Ne <xuandong1405@gmail.com>
2024-07-25 11:51:40 +0700

    up

--- src/API/ApiClient.js
+++ src/API/ApiClient.js
@@ -1,9 +1,24 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 // cho phép axios gửi cookie qua domain khác
-axios.defaults.withCredentials = true;
+
 export const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080',
+    withCredentials: true, // Đảm bảo gửi cookie với mỗi yêu cầu
 });
+
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            // Xử lý lỗi 401 Unauthorized
+            Cookies.remove('jwt'); // Xóa JWT từ cookie nếu cần
+            window.location.href = '/login'; // Chuyển hướng người dùng đến trang đăng nhập
+        }
+        return Promise.reject(error);
+    }
+);
 
 
 export const registerUser = async (username, email, password) => {
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -8,40 +7,33 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
-import banner_mens from './Components/assets/banner_mens.png'
-import banner_womens from './Components/assets/banner_women.png'
-import banner_kids from './Components/assets/banner_kids.png'
-
-// import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
-
-// import { PostList } from './posts';
+import banner_mens from './Components/assets/banner_mens.png';
+import banner_womens from './Components/assets/banner_women.png';
+import banner_kids from './Components/assets/banner_kids.png';
+import SessionHandler from './Session/SessionHandler'; // Adjust the path as needed
 
 function App() {
-  return (
-    <div> 
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory banner ={banner_mens} category="men" />} />
-          <Route path="/womens" element={<ShopCategory banner ={banner_womens} category="women" />} />
-          <Route path="/kids" element={<ShopCategory banner ={banner_kids} category="kid" />} />
-          <Route path="/product" element={<Product />}>
-            <Route path=":productId" element={<Product />} />
-          </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path ='/login' element={<LoginSignup />} />
-        </Routes>
-        <Footer/>
-
-      </BrowserRouter>
-
-{/* <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
-        <Resource name="posts" list={PostList} />
-    </Admin> */}
-
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                {/* <SessionHandler> */}
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Shop />} />
+                        <Route path="/mens" element={<ShopCategory banner={banner_mens} category="men" />} />
+                        <Route path="/womens" element={<ShopCategory banner={banner_womens} category="women" />} />
+                        <Route path="/kids" element={<ShopCategory banner={banner_kids} category="kid" />} />
+                        <Route path="/product" element={<Product />}>
+                            <Route path=":productId" element={<Product />} />
+                        </Route>
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/login" element={<LoginSignup />} />
+                    </Routes>
+                    <Footer />
+                {/* </SessionHandler> */}
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
