0bf3cbe3b5de17d1a426c97b2f139f3dcc7d7e99
=Xuan Dong Ne <xuandong1405@gmail.com>
2024-07-25 23:07:29 +0700

    up

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
-import React, { useContext, useState } from 'react';
+import React, {  useState } from 'react';
 import './Navbar.css';
 import { RxDividerVertical } from "react-icons/rx";
 import { Link } from 'react-router-dom';
-import { ShopContext } from '../../Context/ShopContext';
 import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
@@ -16,11 +15,11 @@ const Navbar = () => {
         <>
         <div className="navbar">
             <ul className='nav-menu'>
-                <li onClick={() => {setMenu('shop')}}><Link style={{textDecoration: 'none', color:'white'}} to= '/'>Collections</Link> {menu === 'shop' && <hr/>}</li>
-                <RxDividerVertical style={{fontSize:'2rem'}}/>
-                <li onClick={() => {setMenu('mens')}}><Link style={{textDecoration: 'none',color:'white'}} to= '/mens'>Man</Link>{menu === 'mens' && <hr/>}</li>
+                <li onClick={() => {setMenu('mens')}}><Link style={{textDecoration: 'none', color:'white'}} to= '/mens'>Man</Link> {menu === 'mens' && <hr/>}</li>
                 <RxDividerVertical style={{fontSize:'2rem'}}/>
                 <li onClick={() => {setMenu('womens')}}><Link style={{textDecoration: 'none',color:'white'}} to= '/womens'>Women</Link>{menu === 'womens' && <hr/>}</li>
+                <RxDividerVertical style={{fontSize:'2rem'}}/>
+                <li onClick={() => {setMenu('kids')}}><Link style={{textDecoration: 'none',color:'white'}} to= '/kids'>Kid</Link>{menu === 'kids' && <hr/>}</li>
             </ul>
             <div className="nav-login-cart">
                 <SearchBar onSearch={handleSearch} />
